5b7a3847fc834bc6dcfd34d708b2d0fa3ed77fbf
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-11-17 16:55:46 +0300

    build: configure design system

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ['Pacifico', ...fontFamily.serif],
+        primary: ['var(--font-primary)', ...fontFamily.serif],
+        secondary: ['var(--font-secondary)', ...fontFamily.sans],
+      },
+      colors: {
+        neutral: 'hsl(var(--color-neutral) / <alpha-value>)',
+        primary: 'hsl(var(--color-primary) / <alpha-value>)',
+        secondary: 'hsl(var(--color-secondary) / <alpha-value>)',
+        surface: 'hsl(var(--color-surface) / <alpha-value>)',
+        'surface-container':
+          'hsl(var(--color-surface-container) / <alpha-value>)',
+        outline: 'hsl(var(--color-outline) / <alpha-value>)',
+        divider: 'hsl(var(--color-divider) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 };

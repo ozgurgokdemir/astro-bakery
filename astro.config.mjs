82d16688cf8f3a9d435994437de1de4f27c10157
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-11-19 19:34:10 +0300

    build: setup icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
 export default defineConfig({
   integrations: [
     tailwind({
       config: {
         applyBaseStyles: false,
+      },
+    }),
+    icon({
+      include: {
+        heroicons: ['arrow-long-right-20-solid'],
+        fe: ['*'],
       },
     }),
   ],

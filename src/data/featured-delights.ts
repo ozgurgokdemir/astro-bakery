11fb075344d40b0099a00e885edc62be714b9395
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-12-04 19:15:25 +0300

    refactor: create data folder

---
+++ src/data/featured-delights.ts
@@ -0,0 +1,26 @@
+import cinnamonRollsImage from '@/assets/cinnamon-rolls.jpg';
+import artisanalBreadImage from '@/assets/artisanal-bread.jpg';
+import sugarCookiesImage from '@/assets/sugar-cookies.jpg';
+
+const featuredDelights = [
+  {
+    image: cinnamonRollsImage,
+    title: 'Signature Cinnamon Rolls',
+    description:
+      'Our freshly baked cinnamon rolls are a customer favorite. Made from a secret family recipe, they are generously smothered in sweet cream cheese icing.',
+  },
+  {
+    image: artisanalBreadImage,
+    title: 'Artisanal Bread Selection',
+    description:
+      'Our selection of artisanal bread, including sourdough, whole grain, and ciabatta, is crafted with care, delivering a perfect balance of crusty exterior and soft, fluffy interior.',
+  },
+  {
+    image: sugarCookiesImage,
+    title: 'Butterfly Sugar Cookies',
+    description:
+      "These beautifully decorated sugar cookies come in various shapes and colors. They're perfect for special occasions or just to satisfy a sweet craving.",
+  },
+];
+
+export default featuredDelights;
---
+++ src/data/opening-hours.ts
@@ -0,0 +1,7 @@
+const openingHours = [
+  { days: 'Monday - Friday', hours: '7:00 AM - 7:00 PM' },
+  { days: 'Saturday', hours: '8:00 AM - 6:00 PM' },
+  { days: 'Sunday', hours: 'Closed' },
+];
+
+export default openingHours;

beead80528a5948d01e95e25bab8f105683e9588
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-12-11 18:10:48 +0300

    chore: add image gallery data

---
+++ src/data/image-gallery.ts
@@ -0,0 +1,17 @@
+import kristenDrewPhoto from '@/assets/kirsten-drew-n49BjsFf5dI.jpg';
+import toaHeftibaPhoto from '@/assets/toa-heftiba-S1Z_Cudra0k.jpg';
+import ulyssePointchevalPhoto from '@/assets/ulysse-pointcheval-1WmlAiYgnoI.jpg';
+import harperNguyenPhoto from '@/assets/harper-nguyen-j6DH45Bflho.jpg';
+import fabioTrainaPhoto from '@/assets/fabio-traina-6i6NJgCJ8k0.jpg';
+import bakeryPhoto from '@/assets/bakery.jpg';
+import karinaKunglaPhoto from '@/assets/karina-kungla-knVYO9dW8_Y.jpg';
+import ddpPhoto from '@/assets/ddp-CceG6jpl19M.jpg';
+import kilyanSockalingumPhoto from '@/assets/kilyan-sockalingum-08EXImz-EYc.jpg';
+
+const imageGallery = {
+  firstColumn: [kristenDrewPhoto, toaHeftibaPhoto, ulyssePointchevalPhoto],
+  secondColumn: [harperNguyenPhoto, fabioTrainaPhoto, bakeryPhoto],
+  thirdColumn: [karinaKunglaPhoto, ddpPhoto, kilyanSockalingumPhoto],
+};
+
+export default imageGallery;

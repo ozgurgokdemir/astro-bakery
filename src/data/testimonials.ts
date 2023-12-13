ab765445b5cb6691cd8ace7158b8b9417c9d3623
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-12-14 02:00:04 +0300

    refactor(testimonials): improve code structure

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -1,59 +1,65 @@
 import codyFisherImage from '@/assets/cody-fisher.png';
-import kristinWatsonImage from '@/assets/kristin-watson.png';
-import devonLaneImage from '@/assets/devon-lane.png';
-import jennyWilsonImage from '@/assets/jenny-wilson.png';
 import robertFoxImage from '@/assets/robert-fox.png';
-import leslieAlexanderImage from '@/assets/leslie-alexander.png';
 import courtneyHenryImage from '@/assets/courtney-henry.png';
+import jennyWilsonImage from '@/assets/jenny-wilson.png';
+import kristinWatsonImage from '@/assets/kristin-watson.png';
 import theresaWebbImage from '@/assets/theresa-webb.png';
+import devonLaneImage from '@/assets/devon-lane.png';
+import leslieAlexanderImage from '@/assets/leslie-alexander.png';
 import darrellStewardImage from '@/assets/darrell-steward.png';
 
-const testimonials = [
-  {
-    image: codyFisherImage,
-    name: 'Cody Fisher',
-    text: "I can't express how much I adore Sweet Delights Bakery! Every visit feels like a warm hug, and their cinnamon rolls are simply out of this world. The ambiance, the treats, and the team's hospitality make it my go-to place for sweet indulgence.",
-  },
-  {
-    image: kristinWatsonImage,
-    name: 'Kristin Watson',
-    text: "Sweet Delights Bakery isn't just about cakes and pastries; it's about creating cherished moments. From family gatherings to a quick treat for myself, this bakery is where I go. The flavors, the atmosphere, and the staff's smiles never disappoint.",
-  },
-  {
-    image: devonLaneImage,
-    name: 'Devon Lane',
-    text: "As a cake enthusiast, I've visited bakeries worldwide, but Sweet Delights stands out. Their attention to detail and the love they put into every creation is evident in every bite. It's a place that exudes a genuine love for baking.",
-  },
-  {
-    image: jennyWilsonImage,
-    name: 'Jenny Wilson',
-    text: 'Sweet Delights Bakery is a treasure in our community! The aroma of freshly baked treats welcomes you, and each bite is a taste of perfection. The friendly staff and cozy ambiance make every visit a delight.',
-  },
-  {
-    image: robertFoxImage,
-    name: 'Robert Fox',
-    text: "As a loyal customer, I can't get enough of Sweet Delights' heavenly pastries. From the first greeting to the last crumb, the experience is nothing short of exceptional.",
-  },
-  {
-    image: leslieAlexanderImage,
-    name: 'Leslie Alexander',
-    text: 'Sweet Delights Bakery is my go-to for special occasions. The attention to detail and personalized service make it the best bakery in town.',
-  },
-  {
-    image: courtneyHenryImage,
-    name: 'Courtney Henry',
-    text: 'A slice of heaven in every bite! The diverse menu and artisanal touch make this bakery a must-visit. The dedication to quality and the friendly atmosphere keep me coming back for more.',
-  },
-  {
-    image: theresaWebbImage,
-    name: 'Theresa Webb',
-    text: "I stumbled upon this hidden gem, and it's been a delightful journey ever since. The pastries are a work of art, and the personalized service creates a unique and memorable experience.",
-  },
-  {
-    image: darrellStewardImage,
-    name: 'Darrell Steward',
-    text: 'A taste sensation! The cookies are my guilty pleasure, and the warm ambiance makes it the perfect spot for relaxation. Sweet indulgence and friendly faces—what more could you ask for?',
-  },
-];
+const testimonials = {
+  firstColumn: [
+    {
+      image: codyFisherImage,
+      name: 'Cody Fisher',
+      text: "I can't express how much I adore Sweet Delights Bakery! Every visit feels like a warm hug, and their cinnamon rolls are simply out of this world. The ambiance, the treats, and the team's hospitality make it my go-to place for sweet indulgence.",
+    },
+    {
+      image: robertFoxImage,
+      name: 'Robert Fox',
+      text: "As a loyal customer, I can't get enough of Sweet Delights' heavenly pastries. From the first greeting to the last crumb, the experience is nothing short of exceptional.",
+    },
+    {
+      image: courtneyHenryImage,
+      name: 'Courtney Henry',
+      text: 'A slice of heaven in every bite! The diverse menu and artisanal touch make this bakery a must-visit. The dedication to quality and the friendly atmosphere keep me coming back for more.',
+    },
+  ],
+  secondColumn: [
+    {
+      image: jennyWilsonImage,
+      name: 'Jenny Wilson',
+      text: 'Sweet Delights Bakery is a treasure in our community! The aroma of freshly baked treats welcomes you, and each bite is a taste of perfection. The friendly staff and cozy ambiance make every visit a delight.',
+    },
+    {
+      image: kristinWatsonImage,
+      name: 'Kristin Watson',
+      text: "Sweet Delights Bakery isn't just about cakes and pastries; it's about creating cherished moments. From family gatherings to a quick treat for myself, this bakery is where I go. The flavors, the atmosphere, and the staff's smiles never disappoint.",
+    },
+    {
+      image: theresaWebbImage,
+      name: 'Theresa Webb',
+      text: "I stumbled upon this hidden gem, and it's been a delightful journey ever since. The pastries are a work of art, and the personalized service creates a unique and memorable experience.",
+    },
+  ],
+  thirdColumn: [
+    {
+      image: devonLaneImage,
+      name: 'Devon Lane',
+      text: "As a cake enthusiast, I've visited bakeries worldwide, but Sweet Delights stands out. Their attention to detail and the love they put into every creation is evident in every bite. It's a place that exudes a genuine love for baking.",
+    },
+    {
+      image: leslieAlexanderImage,
+      name: 'Leslie Alexander',
+      text: 'Sweet Delights Bakery is my go-to for special occasions. The attention to detail and personalized service make it the best bakery in town.',
+    },
+    {
+      image: darrellStewardImage,
+      name: 'Darrell Steward',
+      text: 'A taste sensation! The cookies are my guilty pleasure, and the warm ambiance makes it the perfect spot for relaxation. Sweet indulgence and friendly faces—what more could you ask for?',
+    },
+  ],
+};
 
 export default testimonials;

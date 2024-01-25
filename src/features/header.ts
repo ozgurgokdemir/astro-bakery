9019d0fc43b099d27211f8c4cbb7a331be90e482
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-01-26 02:15:13 +0300

    refactor: scrollEffect functionality

--- src/features/header.ts
+++ src/features/header.ts
@@ -1,42 +1,63 @@
 import { getBaseFontSize, normalizeNumber } from '@/utils';
 
+const header = document.querySelector('header') as HTMLElement;
+
+const root = document.getElementById('root') as HTMLDivElement;
+
+root.style.paddingTop = `${header.offsetHeight}px`;
+
 const baseFontSize = getBaseFontSize();
 
-export function useHeader(header: HTMLElement) {
+const [initialPadding, scrolledPadding] = calculatePadding(
+  '--initial-padding',
+  '--scrolled-padding',
+  baseFontSize,
+);
+
+const scrollDistance = calculateScrollDistance(initialPadding, scrolledPadding);
+
+let padding = initialPadding;
+
+let opacity = 0;
+
+function calculatePadding(initial: string, scrolled: string, length = 1) {
   const computedStyle = getComputedStyle(header);
 
-  const initialPadding = getPadding(computedStyle, '--initial-padding');
+  const initialPadding = parseInt(computedStyle.getPropertyValue(initial));
 
-  const scrolledPadding = getPadding(computedStyle, '--scrolled-padding');
+  const scrolledPadding = parseInt(computedStyle.getPropertyValue(scrolled));
 
-  const scrollDistance = Math.max((initialPadding - scrolledPadding) * 2, 1);
-
-  let padding = initialPadding;
-
-  let opacity = 0;
-
-  return {
-    updatePadding(scrollY: number) {
-      if (scrollY > scrollDistance && padding === scrolledPadding) return;
-
-      padding = Math.max(initialPadding - scrollY / 2, scrolledPadding);
-
-      header.style.paddingBlock = `${padding / baseFontSize}rem`;
-    },
-    updateOpacity(scrollY: number) {
-      if (scrollY > scrollDistance && opacity === 1) return;
-
-      opacity = normalizeNumber(scrollY, scrollDistance);
-
-      header.style.setProperty('--opacity', opacity.toString());
-    },
-  };
+  return [initialPadding * length, scrolledPadding * length];
 }
 
-function getPadding(computedStyle: CSSStyleDeclaration, property: string) {
-  const propertyValue = computedStyle.getPropertyValue(property);
+function calculateScrollDistance(initial: number, scrolled: number) {
+  const distance = (initial - scrolled) * 2;
 
-  const padding = parseInt(propertyValue.replace('rem', '')) * baseFontSize;
+  return Math.max(distance, 1);
+}
 
-  return padding;
+function updatePadding(scrollY: number) {
+  padding = Math.max(initialPadding - scrollY / 2, scrolledPadding);
+
+  header.style.paddingBlock = `${padding / baseFontSize}rem`;
+}
+
+function updateOpacity(scrollY: number) {
+  opacity = normalizeNumber(scrollY, scrollDistance);
+
+  header.style.setProperty('--opacity', opacity.toString());
+}
+
+export function scrollEffect() {
+  const { scrollY } = window;
+
+  const isScrolled = scrollY > scrollDistance;
+
+  if (!isScrolled || padding !== scrolledPadding) {
+    updatePadding(scrollY);
+  }
+
+  if (!isScrolled || opacity !== 1) {
+    updateOpacity(scrollY);
+  }
 }
